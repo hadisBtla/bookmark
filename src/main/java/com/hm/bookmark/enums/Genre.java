91352fc81624509b10a1a6254552af0076f6f4a6
hadisBtla <hadis.beitolamani@gmail.com>
2023-05-10 21:51:46 +0430

    add enum LifeStatue

--- src/main/java/com/hm/bookmark/enums/Genre.java
+++ src/main/java/com/hm/bookmark/enums/Genre.java
@@ -1,7 +1,9 @@
 package com.hm.bookmark.enums;
 
 public enum Genre {
-    CINEMA,Child,mythological , scary,Criminal,trailer,
-    Magic_realism,Romantic,Science_Fiction,fantasy,classic,comic,adventure;
+    CINEMA,CHILD,MYTHOLOGICAL ,
+    SCARY,CRIMINAL,TRAILER,
+    MAGIC_REALISM,ROMANTIC,SCIENCE_FICTION,
+    FANTASY,CLASSIC,COMIC,ADVENTURE;
 
 }
--- src/main/java/com/hm/bookmark/enums/LifeStatus.java
+++ src/main/java/com/hm/bookmark/enums/LifeStatus.java
@@ -1,4 +1,5 @@
 package com.hm.bookmark.enums;
 
 public enum LifeStatus {
+    LIVE, DEATH;
 }
