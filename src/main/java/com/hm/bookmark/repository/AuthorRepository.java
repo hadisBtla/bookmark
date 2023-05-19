c2c718ee3f6e314d528abdf6a3c21ae1041a877f
Mina <mina.enjedani@outlook.com>
2023-05-19 16:20:37 +0430

    id type  updated in repository layer

--- src/main/java/com/hm/bookmark/repository/AuthorRepository.java
+++ src/main/java/com/hm/bookmark/repository/AuthorRepository.java
@@ -3,5 +3,5 @@ package com.hm.bookmark.repository;
 import com.hm.bookmark.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AuthorRepository extends CrudRepository<Author,String> {
+public interface AuthorRepository extends CrudRepository<Author,Long> {
 }
--- src/main/java/com/hm/bookmark/repository/PublicationRepository.java
+++ src/main/java/com/hm/bookmark/repository/PublicationRepository.java
@@ -3,5 +3,5 @@ package com.hm.bookmark.repository;
 import com.hm.bookmark.domain.Publication;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PublicationRepository extends CrudRepository<Publication,String> {
+public interface PublicationRepository extends CrudRepository<Publication,Long> {
 }
