9270d6f3deab724634eb3f6004e95e29d3e47d3d
Mina <mina.enjedani@outlook.com>
2023-05-19 17:55:17 +0430

    comment service added

--- src/main/java/com/hm/bookmark/service/CommentService.java
+++ src/main/java/com/hm/bookmark/service/CommentService.java
@@ -1,4 +1,22 @@
 package com.hm.bookmark.service;
 
+import com.hm.bookmark.domain.Comment;
+import com.hm.bookmark.repository.CommentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class CommentService {
+
+  private final CommentRepository repository;
+
+  public String saveComment(String comment) {
+    final Comment commentEntity =
+        Comment.builder()
+            .commentOwner("0015338436" /*TODO must get ssn from security context*/)
+            .commentText(comment)
+            .build();
+    return repository.save(commentEntity).getId();
+  }
 }
